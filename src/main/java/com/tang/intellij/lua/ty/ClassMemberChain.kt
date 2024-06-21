1535ae7d8f4d1898e00e82d0009fe819cfaedabf
sqeetyshan <sqeetyshan@tencent.com>
2024-06-21 20:49:49 +0800

    修复多个type定义导致函数跳转没跳到class定义的

--- src/main/java/com/tang/intellij/lua/ty/ClassMemberChain.kt
+++ src/main/java/com/tang/intellij/lua/ty/ClassMemberChain.kt
@@ -16,20 +16,82 @@
 
 package com.tang.intellij.lua.ty
 
-import com.tang.intellij.lua.psi.LuaClassMember
+import com.tang.intellij.lua.comment.psi.LuaDocTagClass
+import com.tang.intellij.lua.comment.psi.impl.LuaCommentImpl
+import com.tang.intellij.lua.comment.psi.impl.LuaDocTagClassImpl
+import com.tang.intellij.lua.ext.stubOrPsiParent
+import com.tang.intellij.lua.psi.*
+import com.tang.intellij.lua.psi.impl.LuaClassMethodDefImpl
+import com.tang.intellij.lua.psi.impl.LuaClassMethodNameImpl
+import com.tang.intellij.lua.psi.impl.LuaLocalDefImpl
+import com.tang.intellij.lua.psi.impl.LuaNameExprImpl
+import kotlinx.html.Entities
 
 class ClassMemberChain(val ty: ITyClass, var superChain: Array<ClassMemberChain>) {
     private val members = mutableMapOf<String, LuaClassMember>()
-
+    private val builtinMembers = mutableSetOf<String>()
     fun add(member: LuaClassMember) {
         val name = member.name ?: return
         val superExist = findSuperMember(name)
         val override = superExist == null || canOverride(member, superExist)
         if (override) {
             val selfExist = members[name]
-            if (selfExist == null || member.worth > selfExist.worth)
+            if (selfExist == null)
+            {
                 members[name] = member
+                if(isClassDefineMember(member)){
+                    builtinMembers.add(name)
+                }
+            }
+            else{
+                if(member.worth > selfExist.worth){
+                    members[name] = member
+                }
+                if(!builtinMembers.contains(name)){
+                    if(isClassDefineMember(member)){
+                        builtinMembers.add(name)
+                        members[name] = member
+                    }
+                }
+            }
         }
+    }
+
+    private fun isClassDefineMember(member: LuaClassMember):Boolean{
+        val className = getClassDefineName(member)
+        var prev = member.prevSibling
+        while(prev != null) {
+            if(prev is LuaLocalDef) {
+                val localName = prev.nameList?.firstChild?.text
+                if(localName == className){
+                    prev.children.forEach { psiElement ->
+                        if(psiElement is LuaCommentImpl) {
+                            val docTag = psiElement.findTag(LuaDocTagClass::class.java)
+                            if (docTag != null) {
+                                val type = docTag.type
+                                if(type.className == ty.className) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                    return false
+                }
+            }
+            prev = prev.prevSibling
+        }
+        return false
+    }
+
+    private fun getClassDefineName(member: LuaClassMember):String?{
+        if(member is LuaClassMethodDefImpl){
+            val luaClassMethodName = member.children.find { it is LuaClassMethodNameImpl }
+            val luaName = luaClassMethodName?.firstChild
+            if (luaName is LuaNameExprImpl) {
+                return luaName.text
+            }
+        }
+        return null
     }
 
     fun findSuperMember(name: String): LuaClassMember? {
