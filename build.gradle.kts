9a877c6f4d599011a0ffe6690d1387053114746e
sqeety <283963287@qq.com>
2025-04-23 23:10:21 +0800

    增加2025支持

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ import java.io.ByteArrayOutputStream
 
 plugins {
     id("org.jetbrains.intellij").version("1.13.3")
-    id("org.jetbrains.kotlin.jvm").version("1.8.21")
+    id("org.jetbrains.kotlin.jvm").version("2.1.20")
     id("de.undercouch.download").version("5.3.0")
 }
 
